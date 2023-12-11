e3dab45ff8d39af5c0a29a75b3c8c973055328b5
Prachi deokate <136556414+DailyBiteMeals@users.noreply.github.com>
2023-12-11 19:55:26 +0530

    deployed link updated

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Credentials",
+    "https://dailybite4-0-backend.onrender.com/"
+  );
   next();
 });
 

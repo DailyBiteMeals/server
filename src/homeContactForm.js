aa86b60abec2b165657769acda4699832182887f
Prachi deokate <136556414+DailyBiteMeals@users.noreply.github.com>
2023-12-12 12:47:11 +0530

    Update homeContactForm.js

--- src/homeContactForm.js
+++ src/homeContactForm.js
@@ -5,11 +5,9 @@ import sheets, { SHEET_ID } from "./sheetClient1.js";
 const router = express.Router();
 
 const homeContactFormSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
-  email: z.string().email(1, { message: "Email is required" }),
-  mobile: z.string().refine((value) => /^\d{10}$/g.test(value), {
-    message: "Mobile number should be 10 digits",
-  }),
+  name: z.string().min(1).max(255),
+  email: z.string().email(),
+  mobile: z.string().regex(/^\d{10}$/),
   message: z.string().optional(),
 });
 
@@ -19,23 +17,31 @@ router.post("/HomeContactForm", async (req, res) => {
 
     // Object to Sheets
     const rows = Object.values(body);
-    console.log(rows);
+    console.log("Rows to be added to Google Sheets:", rows);
 
-    await sheets.spreadsheets.values.append({
+    // Append data to Google Sheets
+    const sheetsResponse = await sheets.spreadsheets.values.append({
       spreadsheetId: SHEET_ID,
-      range: "Home!A2:D2",
+      range: "Home!A2:D2", // Adjust the range as per your Sheets structure
       insertDataOption: "INSERT_ROWS",
       valueInputOption: "RAW",
       requestBody: {
         values: [rows],
       },
     });
-    res.json({ message: "Data added successfully" });
+
+    // If successful response from Google Sheets API
+    res.json({ message: "Data added successfully", sheetsResponse });
   } catch (error) {
     if (error instanceof ZodError) {
-      res.status(400).json({ error: error.message });
+      // Schema validation error
+      res
+        .status(400)
+        .json({ error: "Validation failed", details: error.errors });
     } else {
-      res.status(400).json({ error });
+      // Other errors, including Google Sheets API errors
+      console.error("Error occurred:", error);
+      res.status(500).json({ error: "Internal server error" });
     }
   }
 });
